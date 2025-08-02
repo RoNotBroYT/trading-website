84007297986deb909a8d5976c230ae06c3610b78
RoNotBroYT <ronotbro@gmail.com>
2025-08-02 11:07:56 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,1 +1,18 @@
+import '../styles/globals.css';
+
+export const metadata = {
+  title: 'Ro-Trades',
+  description: 'Follow our list of hot stocks every day!',
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body>
+        <div className="bg-circles" />
+        {children}
+      </body>
+    </html>
+  );
+}
 
